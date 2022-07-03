9e3a692d5ee8e7e4eb84a729524a5031c0219496
Chris Blake <chris.a.g.blake@gmail.com>
2022-07-04 09:40:22 +1200

    only install anaconda and not any other libs

---
+++ setup_ec2_anaconda.sh
@@ -0,0 +1,16 @@
+# script to setup an EC2 instance with the anaconda version of python
+
+# initial update
+sudo apt update
+sudo apt install cmake -y
+
+# python
+if [ $(uname -m) == aarch64 ]; then
+    wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-aarch64.sh
+    bash Anaconda3-2022.05-Linux-aarch64.sh
+else
+    wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+    bash Anaconda3-2022.05-Linux-x86_64.sh
+fi
+anaconda3/bin/conda init bash
+
